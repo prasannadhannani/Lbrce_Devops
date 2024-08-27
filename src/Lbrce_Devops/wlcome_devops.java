4c99effa2c7f5379ee5704583c566bab528184f0
AdmiN <AdmiN@ITWS0017>
2024-08-27 11:18:53 +0530

    commit

--- src/Lbrce_Devops/wlcome_devops.java
+++ src/Lbrce_Devops/wlcome_devops.java
@@ -2,6 +2,5 @@ package Lbrce_Devops;
 public class wlcome_devops {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to DevopsACD pipeline");
+		System.out.println("Welcome to Devops ACD pipeline");
 	}
-}
