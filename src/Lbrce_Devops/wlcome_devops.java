dd974e9c5f4577d12563b68c32200caa536257ab
AdmiN <AdmiN@ITWS0017>
2024-08-27 09:55:33 +0530

    commit

--- src/Lbrce_Devops/wlcome_devops.java
+++ src/Lbrce_Devops/wlcome_devops.java
@@ -2,6 +2,6 @@ package Lbrce_Devops;
 public class wlcome_devops {
 
 	public static void main(String[] args) {
-		 System.out.println("Welcome to DevOps Lab");
+		 System.out.println("Welcome to DL");
 	}
 }
